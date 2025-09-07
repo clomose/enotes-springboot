f612d3e4790f9af41ea2bab2669ee2370c401703
Utkarsh Pant <smartbroutkarsh@gmail.com>
2025-09-07 21:52:58 +0530

    copy notes

--- src/main/java/com/example/Enotes/entity/Notes.java
+++ src/main/java/com/example/Enotes/entity/Notes.java
@@ -1,10 +1,7 @@
 package com.example.Enotes.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Notes extends BaseModel {
 
     @Id
--- src/main/java/com/example/Enotes/service/NotesService.java
+++ src/main/java/com/example/Enotes/service/NotesService.java
@@ -35,4 +35,6 @@ public interface NotesService {
     public void unFavouriteNotes(Integer noteId) throws Exception;
 
     public List<FavouriteNoteDto> getUserFavouriteNotes() throws Exception;
+
+    public Boolean copyNotes(Integer id) throws Exception;
 }
