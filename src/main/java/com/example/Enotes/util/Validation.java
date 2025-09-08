a07c0b4ac4aec501ca9c2b84f6c3c3afaeeb45bf
Utkarsh Pant <smartbroutkarsh@gmail.com>
2025-09-08 15:20:51 +0530

    user registration implementation

--- src/main/java/com/example/Enotes/util/Validation.java
+++ src/main/java/com/example/Enotes/util/Validation.java
@@ -2,17 +2,32 @@ package com.example.Enotes.util;
 
 import com.example.Enotes.dto.CategoryDto;
 import com.example.Enotes.dto.TodoDto;
+import com.example.Enotes.dto.UserDto;
+import com.example.Enotes.entity.Role;
 import com.example.Enotes.enums.TodoStatus;
+import com.example.Enotes.exception.ResourceAlreadyExists;
 import com.example.Enotes.exception.ResourceNotFoundException;
 import com.example.Enotes.exception.ValidationException;
+import com.example.Enotes.repository.RoleRepository;
+import com.example.Enotes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class Validation {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public void categoryValidation(CategoryDto  categoryDto){
         Map<String,Object> error = new HashMap<>();
@@ -69,6 +84,40 @@ public class Validation {
         }
         if(!statusFound){
             throw new ResourceNotFoundException("invalid status");
+        }
+    }
+
+    public void userValidation(UserDto userDto) throws Exception{
+        if (!StringUtils.hasText(userDto.getFirstName())){
+            throw new IllegalArgumentException("First name is invalid");
+        }
+        if (!StringUtils.hasText(userDto.getLastName())){
+            throw new IllegalArgumentException("Last name is invalid");
+        }
+        if (!StringUtils.hasText(userDto.getEmail()) || !userDto.getEmail().matches(Constants.EMAIL_REGEX)){
+            throw new IllegalArgumentException("email is invalid");
+        }else{
+            Boolean existEmail = userRepository.existsByEmail(userDto.getEmail());
+            if(existEmail){
+                throw new ResourceAlreadyExists("Mail Id already exist");
+            }
+        }
+        if (!StringUtils.hasText(userDto.getMobNo()) || !userDto.getMobNo().matches(Constants.MOBNO_REGEX)){
+            throw new IllegalArgumentException("mobile number is invalid");
+        }
+
+        if(CollectionUtils.isEmpty(userDto.getRoles())){
+            throw new IllegalArgumentException("Role is invalid");
+        }
+
+        List<Integer> roleIds = roleRepository.findAll().stream().map(r -> r.getId()).toList();
+
+        List<Integer> reqRoleids = userDto.getRoles().stream()
+                .map(r -> r.getId())
+                .filter(roleId -> !roleIds.contains(roleId)).toList();
+
+        if(!CollectionUtils.isEmpty(reqRoleids)){
+            throw new IllegalArgumentException("Role is invalid ");
         }
     }
 }
