1b4c674686d5b7852f598c16520dc26994ba18b6
Utkarsh Pant <smartbroutkarsh@gmail.com>
2025-09-10 00:38:07 +0530

    token generate implemented

---
+++ src/main/java/com/example/Enotes/ServiceImpl/JwtServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.Enotes.ServiceImpl;
+
+import com.example.Enotes.entity.User;
+import com.example.Enotes.service.JwtService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    private String secretKey="";
+
+    public JwtServiceImpl(){
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String generateToken(User user) {
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id",user.getId());
+        claims.put("role",user.getRoles());
+        claims.put("status",user.getStatus().getIsActive());
+        String token = Jwts.builder()
+                .claims().add(claims) //information that is to be stored
+                .subject(user.getEmail()) //name of the field name
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+60*60*10))
+                .and()
+                .signWith(getKey())
+                .compact();
+        return token;
+    }
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
