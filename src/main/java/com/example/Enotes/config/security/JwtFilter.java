061d9f2fd9e657534a08700d8f1b78a3d840c866
Utkarsh Pant <smartbroutkarsh@gmail.com>
2025-09-11 22:55:43 +0530

    role based authentication

--- src/main/java/com/example/Enotes/config/security/JwtFilter.java
+++ src/main/java/com/example/Enotes/config/security/JwtFilter.java
@@ -1,11 +1,14 @@
 package com.example.Enotes.config.security;
 
+import com.example.Enotes.handler.GenericResponse;
 import com.example.Enotes.service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,28 +30,44 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    try {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String email = null;
-        if (authHeader!=null && authHeader.startsWith("Bearer ")){
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.split(" ")[1];
             email = jwtService.extractEmail(token);
         }
 
-        if (email!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-            Boolean validateToken = jwtService.validateToken(token,userDetails);
-            if (validateToken){
+            Boolean validateToken = jwtService.validateToken(token, userDetails);
+            if (validateToken) {
                 UsernamePasswordAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(userDetails,
-                                null,userDetails.getAuthorities());
+                                null, userDetails.getAuthorities());
                 //Adds extra details (IP address, session ID).
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
-
+    }catch (Exception e){
+        generateErrorResponse(response,e);
+        return;
+    }
         filterChain.doFilter(request,response);
+    }
+
+    private void generateErrorResponse(HttpServletResponse response, Exception e) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        Object error = GenericResponse.builder()
+                .status("failed")
+                .message(e.getMessage())
+                .responseStatus(HttpStatus.UNAUTHORIZED)
+                .build()
+                .create()
+                .getBody();
+        response.getWriter().write(new ObjectMapper().writeValueAsString(error));
     }
 }
--- src/main/java/com/example/Enotes/controller/CategoryController.java
+++ src/main/java/com/example/Enotes/controller/CategoryController.java
@@ -9,6 +9,7 @@ import com.example.Enotes.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,7 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @PostMapping("/save")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> saveCategory(@RequestBody CategoryDto categoryDto){
         Boolean saveCategory = categoryService.saveCategory(categoryDto);
         if(saveCategory){
@@ -35,6 +37,7 @@ public class CategoryController {
     }
 
     @GetMapping("/")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAllCategory(){
         List<CategoryDto> categoriesDto = categoryService.getAllCategory();
         if(CollectionUtils.isEmpty(categoriesDto)){
@@ -45,6 +48,7 @@ public class CategoryController {
     }
 
     @GetMapping("/active")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public ResponseEntity<?> getActiveCategory(){
         List<CategoryResponse> categoriesDto = categoryService.getActiveCategory();
         if(CollectionUtils.isEmpty(categoriesDto)){
@@ -55,6 +59,7 @@ public class CategoryController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getCategoryDetailsById(@PathVariable Integer id) throws Exception{
         //Without try catch Global Error Handler will execute
         CategoryDto categoryDto = categoryService.getCategoryById(id);
@@ -68,6 +73,7 @@ public class CategoryController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deleteCategoryById(@PathVariable Integer id){
         Boolean deleted = categoryService.deleteCategoryById(id);
         if(deleted){
