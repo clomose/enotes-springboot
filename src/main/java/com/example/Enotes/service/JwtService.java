260f2716837a51a88b84d01a2774827c03638844
Utkarsh Pant <smartbroutkarsh@gmail.com>
2025-09-11 21:23:42 +0530

    validate jwt

--- src/main/java/com/example/Enotes/service/JwtService.java
+++ src/main/java/com/example/Enotes/service/JwtService.java
@@ -1,7 +1,12 @@
 package com.example.Enotes.service;
 
 import com.example.Enotes.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface JwtService {
     public String generateToken(User user);
+
+    public String extractEmail(String  token);
+
+    public Boolean validateToken(String token, UserDetails userDetails);
 }
