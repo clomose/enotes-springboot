9e2da33f0767b10d364c03aeafc1f72aac813ea6
Utkarsh Pant <smartbroutkarsh@gmail.com>
2025-09-13 12:34:06 +0530

    password reset implemented

--- src/main/java/com/example/Enotes/service/UserService.java
+++ src/main/java/com/example/Enotes/service/UserService.java
@@ -1,7 +1,15 @@
 package com.example.Enotes.service;
 
 import com.example.Enotes.dto.PasswordChangeRequest;
+import com.example.Enotes.dto.PswdResetRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface UserService {
      public void changePassword(PasswordChangeRequest passwordChangeRequest);
+
+     void sendEmailPasswordReset(String email, HttpServletRequest request) throws Exception;
+
+     void verifyPswdResetLink(Integer uid, String code) throws Exception;
+
+     void resetPassword(PswdResetRequest pswdResetRequest) throws Exception;
 }
